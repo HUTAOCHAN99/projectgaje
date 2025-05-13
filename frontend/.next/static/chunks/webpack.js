c02ce3444d8254a8ab2cab08de62315594e66c13
rahadityaputra <rahadit.a.p123@gmail.com>
2025-05-13 07:20:09 +0700

    fix tailwind

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7b37eaf6b0d080f0"; }
+/******/ 		__webpack_require__.h = function() { return "71920b045cae8104"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
